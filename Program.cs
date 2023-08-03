868c9de264eb32fa9f029711b4019fb25b38d765
VladislavAndreev <andreev.92@inbox.ru>
2023-08-03 14:59:17 +0300

    edit2_Task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
 ﻿string[] firstArray = new string[5] { "Hello", "2", "world", ":-)", "qte" };
 string[] secondArray = new string[firstArray.Length];
+void SecondArray(string[] firstArray, string[] secondArray)
+{
+    int count = 0;
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+        if (firstArray[i].Length <= 3)
+        {
+            secondArray[count] = firstArray[i];
+            count++;
+        }
+    }
+}
+SecondArray(firstArray, secondArray);
+Console.WriteLine($"Исходный массив: [{string.Join(", ", firstArray)}]");
+Console.WriteLine($"Отсортированный массив: [{string.Join(", ", secondArray)}]");
